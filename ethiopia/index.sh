1046ca5208dc4ebb96f7228f0837acd12fbd9ccc
RichRico <richman.pariona@gmail.com>
2018-07-27 15:26:26 -0500

    update files

--- ethiopia/index.sh
+++ ethiopia/index.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-echo "info"
 	while IFS='' read -r line || [[ -n "$line" ]]; do
 
 	    echo {\"type\": \"FeatureCollection\", \"features\": [$line]} > zone.geojson
-	    zoneName="$(node index.js zone.geojson)"
-	    echo $zoneName > name.$$.json
+	    #zoneName="$(node index.js zone.geojson)"
 	    #cp zone.geojson $zoneName.geojson > name.json
 	    geojson2poly zone.geojson zone.poly
 	    osmconvert ethiopia-latest.osm.pbf -B=zone.poly --complete-ways --complex-ways -o=region.pbf
 	    #node /home/ds/apps/osmlazer/index.js --file region.pbf --mode basemap | jq .
 	    info="$(node /home/ds/apps/osmlazer/index.js --file region.pbf --mode poi)"
-    	echo $info > info.$$.json
+    	echo $info > info.json
+    	result = "$(node merge.js zone.geojson info.json)"
+
+    	echo $result >> outpt.json
+
+
     
 	done < res.json
